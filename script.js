7eb5a434f9b3d69e92686ecf6aa596bd525f97c7
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-14 23:41:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function stringChop(str, size) {
   // your code here
+	let ans = [];
+    for(i=0;i<str.length;i+=size){
+        ans.push(str.slice(i,i+size));
+    }
+    return ans;
 }
 
 // Do not change the code below
